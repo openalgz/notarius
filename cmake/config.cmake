04adfa564caea5d86b9fbaf0a9fd97b84e32522e
bill.berry <bill.berry@matrixresearch.com>
2024-06-06 16:18:41 -0500

    Fixed CMake Issues w/spdlog

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -17,36 +17,36 @@ set(PROJECT_URL "https://github.com/openalgz/notarius" CACHE INTERNAL "")
 macro(generate_notarius)
 
    if (PROJECT_IS_TOP_LEVEL)
-   #[[
+   
       list(APPEND PROJECT_DEPENDENCIES_LIST "spdlog, https://github.com/gabime/spdlog.git, v1.13.0")
       list(APPEND PROJECT_DEPENDENCIES_LIST "ut,     https://github.com/boost-ext/ut.git,  v2.0.1")
-   #]]
+   
       configure_boost_micro_unit_testing()
       fetch_content_and_make_available("${PROJECT_DEPENDENCIES_LIST}")   
+
       include("${CMAKE_DIR}/core/dev-mode.cmake")
+
    else()
       make_header_only_project()
+
    endif()
    
 endmacro()
 
-
 macro(suppress_compiler_warnings)
-#[[
-   if (PROJECT_IS_TOP_LEVEL)
-      if (MSVC)
-         target_compile_options(spdlog PRIVATE /W0)
-         add_definitions(-D_CRT_SECURE_NO_WARNINGS -D_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS)
-      elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang|GNU")
+
+   if (MSVC)
+      target_compile_options(spdlog PRIVATE /W0)
+      add_definitions(-D_CRT_SECURE_NO_WARNINGS -D_SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS)
+
+   elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang|AppleClang|GNU")
+      if (TARGET spdlog)
          target_compile_options(spdlog PRIVATE -w)
       endif()
+
    endif()
-#]]
-message("") 
 
-# fix this!!!!!
 endmacro()
-
 
 if (@SUPRESS_COMPILER_WARNING)
    suppress_compiler_warnings()
--- cmake/core/project-is-top-level.cmake
+++ cmake/core/project-is-top-level.cmake
@@ -1,7 +1,10 @@
-# This variable is set by project() in CMake 3.21+
-if(CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
-   set(IS_TOPLEVEL_PROJECT ON CACHE INTERNAL "" FORCE)
-else()
-   set(IS_TOPLEVEL_PROJECT OFF CACHE INTERNAL "" FORCE)
-endif()
+macro(is_topmost_project)
+   # This variable is set by project() in CMake 3.21+
+   if(CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
+      set(PROJECT_IS_TOP_LEVEL ON CACHE INTERNAL "" FORCE)
+   else()
+      set(PROJECT_IS_TOP_LEVEL OFF CACHE INTERNAL "" FORCE)
+   endif()
+endmacro()
 
+is_topmost_project()
