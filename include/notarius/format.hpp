f51b3ef4b792a5777510bf109e98e0095ed1145c
Bill Berry <wberry.cpp@gmail.com>
2024-07-02 21:28:41 -0500

    Updated io utils.

--- include/notarius/format.hpp
+++ include/notarius/format.hpp
@@ -13,19 +13,31 @@ namespace slx
    template <typename T>
    concept is_numeric = std::is_arithmetic_v<T>;
 
-   template <typename First, typename... Args>
-   auto fmt_string(First&& first, Args&&... args)
-   {
-      return std::vformat(std::forward<First>(first), std::make_format_args(args...));
-   }
+  template <typename... Args>
+  std::string fmt_string(std::string_view format_str, Args&&... args)
+  {
+    return std::vformat(format_str, std::make_format_args(std::forward<Args>(args)...));
+  }
+  
+  // Overload for compile-time format string checking (C++20 and later)
+  //
+  template <typename... Args>
+  constexpr auto fmt_string(const std::format_string<Args...>& format_str, Args&&... args)
+  {
+    return std::format(format_str, std::forward<Args>(args)...);
+  }
 
-   template <is_numeric T>
-   std::string to_string(const T value, const int precision = 4)
-   {
-      static thread_local std::ostringstream out;
-      out.str("");
-      out << std::fixed << std::setprecision(precision) << value;
-      return out.str();
-   }
+  template <is_numeric T>
+  std::string to_string(const T value, const int precision = 4)
+  {
+    static thread_local std::ostringstream out;
+    out.str("");
+    out.clear();  // Clear any error flags
+    if constexpr (std::is_floating_point_v<T>) {
+      out << std::fixed << std::setprecision(precision);
+    }
+    out << value;
+    return out.str();
+  }
 
 }
