8135c67ba74892a646c4ae3cfcc503b14982f8b1
wsberry <wsberry@users.noreply.github.com>
2024-07-12 15:51:04 +0000

    Committing clang-format changes

--- src/demo/src/tests/notarius-demo.cpp
+++ src/demo/src/tests/notarius-demo.cpp
@@ -4,8 +4,8 @@
 constexpr auto test_runs_to_perform = 5;
 constexpr auto max_lines_count_ = 10;
 
-#include "ut/ut.hpp"
 #include "notarius/notarius.hpp"
+#include "ut/ut.hpp"
 
 using namespace ut;
 using namespace slx; // the notarius namespace
@@ -13,8 +13,9 @@ using namespace slx; // the notarius namespace
 // Note:
 // I recommend using a Markdown editor for reviewing result files.
 
-suite example_notarius_use_cases = [] { notarius_t<"demo-log.md", notarius_opts_t{.enable_file_logging = true}> demo;
-   
+suite example_notarius_use_cases = [] {
+   notarius_t<"demo-log.md", notarius_opts_t{.enable_file_logging = true}> demo;
+
    demo("Hello World");
    auto actual = demo.str();
    expect("Hello World" == actual);
@@ -23,15 +24,6 @@ suite example_notarius_use_cases = [] { notarius_t<"demo-log.md", notarius_opts_
    auto expected_result = std::format("Hello World Again {} {} {} {}", 1.23, 2.23, 3.23, 4.23);
    actual = demo.str();
    expect(actual == expected_result);
+};
 
-
-
-   
-   };
-
-int main()
-{
-
-   return 0;
-}
-
+int main() { return 0; }
