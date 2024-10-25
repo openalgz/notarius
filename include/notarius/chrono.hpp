e84d2738b7d619951e570f25e94ae2970aa3360a
wsberry <wsberry@users.noreply.github.com>
2024-10-25 16:42:05 +0000

    Committing clang-format changes

--- include/notarius/chrono.hpp
+++ include/notarius/chrono.hpp
@@ -44,9 +44,15 @@ namespace slx
          const int64_t nanoseconds = duration_ns % ns_per_ms;
 
          if (nanoseconds > 0) {
-            return std::format("{:02}<span style = \"color:#B22222\">m</span> {:02}<span style = \"color:#B22222\">s</span> {:03}<span style = \"color:#B22222\">ms</span> {:09}<span style = \"color:#B22222\">ns</span>", minutes, seconds, milliseconds, nanoseconds);
+            return std::format(
+               "{:02}<span style = \"color:#B22222\">m</span> {:02}<span style = \"color:#B22222\">s</span> {:03}<span "
+               "style = \"color:#B22222\">ms</span> {:09}<span style = \"color:#B22222\">ns</span>",
+               minutes, seconds, milliseconds, nanoseconds);
          }
-         return std::format("{:02}<span style = \"color:#B22222\">m</span> {:02}<span style = \"color:#B22222\">s</span> {:03}<span style = \"color:#B22222\">ms</span>", minutes, seconds, milliseconds);
+         return std::format(
+            "{:02}<span style = \"color:#B22222\">m</span> {:02}<span style = \"color:#B22222\">s</span> {:03}<span "
+            "style = \"color:#B22222\">ms</span>",
+            minutes, seconds, milliseconds);
       }
 
       // returns: yyyymmdd::hh:mm:ss
